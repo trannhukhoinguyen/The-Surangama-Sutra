727d02a132ac4c26080b4cc450283d8d15b83a8b
trann <trannhukhoinguyen@gmail.com>
2025-05-26 15:22:24 +0700

    update content, add component Message.astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -17,6 +17,7 @@ import { GithubCardComponent } from "./src/plugins/rehype-component-github-card.
 import { parseDirectiveNode } from "./src/plugins/remark-directive-rehype.js";
 import { remarkExcerpt } from "./src/plugins/remark-excerpt.js";
 import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
@@ -24,6 +25,7 @@ export default defineConfig({
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
+		mdx(),
 		tailwind({
 			nesting: true,
 		}),
